474761181943fb019f44993211d47cb762f3170d
edwardhome <edward61221@gmail.com>
2020-12-08 00:14:19 +0800

    update

---
+++ ClassforBMI/health.py
@@ -0,0 +1,17 @@
+class Human:
+    def __init__(self,h=0,m=0):
+        self.height = h
+        self.weight = m
+    def BMI(self):
+        return self.weight/(self.height/100)**2
+    def BMItype(self,maxmum=25,minmum=18):
+        if self.BMI()> maxmum:
+            return '過重'
+        elif self.BMI() <minmum:
+            return '過輕'
+        else:
+            return '正常'
+    
+
+        
+            
--- ClassforBMI/main.py
+++ ClassforBMI/main.py
@@ -1,8 +1,8 @@
-from human import Human
+from health import Human
 
 Tony = Human(150,45)
 Tim = Human(180,75)
 John = Human(168,95)
 Emily = Human(162,44)
 print(Tony.BMI(),Tim.BMI(),John.BMI(),Emily.BMI())
-print(Tony.health(),Tim.health(),John.health(),Emily.health())
+print(Tony.BMItype(),Tim.BMItype(),John.BMItype(),Emily.BMItype())

aac2edfa0ad6e572f0e85e55bd7b440b7106ca0c
edwardhome <edward61221@gmail.com>
2020-12-08 11:50:57 +0800

    update

--- turtleandfish/turtleandfish.py
+++ turtleandfish/turtleandfish.py
@@ -1,16 +1,19 @@
 import random
 class Animal(object):
     def __init__(self):
+        #定義初始位置
         self.x = random.randint(1,10)
         self.y = random.randint(1,10)
 
     def move(self,move_skill):
+        #提供移動條件
         new_x = self.x + random.choice(move_skill)
         new_y = self.y + random.choice(move_skill)
         self.x = self.is_vaild(new_x)
         self.y = self.is_vaild(new_y)
 
     def is_vaild(self,value):
+        #設定邊界條件
         if 1 <= value <= 10:
             return value
         elif value<1:
@@ -19,7 +22,7 @@ class Animal(object):
             return 10-(value-10)
 
 
-class Turtle(Animal):
+class Turtle(Animal):#繼承動物類別
 
     def __init__(self):
         super(Turtle,self).__init__()
@@ -31,7 +34,7 @@ class Turtle(Animal):
     def eat(self):
         self.power += 20
 
-class Fishs(Animal):
+class Fishs(Animal):#繼承動物類別
 
     def move(self,move_skill=(-1,0,1)):
         super(Fishs,self).move(move_skill)
